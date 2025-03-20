c41668a14a1c679b977f2a8858b6f4d48b0088ba
jayelmeynak <magomed110301@yandex.ru>
2025-03-20 05:42:20 +0300

    optimized intersect

--- intersect.py
+++ intersect.py
@@ -2,14 +2,27 @@ from typing import List
 
 
 class Solution:
+    # def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    #     res = []
+    #     for i in range(len(nums1)):
+    #         if nums1[i] in nums2:
+    #             res.append(nums1[i])
+    #             nums2.remove(nums1[i])
+    #     return res
+
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         res = []
-        for i in range(len(nums1)):
-            if nums1[i] in nums2:
-                res.append(nums1[i])
-                nums2.remove(nums1[i])
+        dic1 = {}
+        dic2 = {}
+        for num1 in nums1:
+            dic1[num1] = dic1.get(num1, 0) + 1
+        for num2 in nums2:
+            dic2[num2] = dic2.get(num2, 0) + 1
+        for key in dic1.keys():
+            repeat = min(dic1[key], dic2.get(key, 0))
+            for i in range(repeat):
+                res.append(key)
         return res
-
 
 if __name__ == '__main__':
     s = Solution()
